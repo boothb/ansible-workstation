3e4f0226db0244f31573825ca54d43cad0b6f25c
boothby <marvin.boothby@posti.io>
2020-11-18 08:28:26 +0100

    Test before commit

--- roles/bash/files/common.bash
+++ roles/bash/files/common.bash
@@ -34,6 +34,6 @@ alias la='ls -A'
 alias l='ls -CF'
 
 # Automatic tmux session
-if [[ $- = *i* ]] && && [[ -z "$TMUX" ]] && [[ -n "$SSH_CONNECTION" ]]; then
+if [[ $- = *i* ]] && [[ -z "$TMUX" ]] && [[ -n "$SSH_CONNECTION" ]]; then
   tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
 fi
